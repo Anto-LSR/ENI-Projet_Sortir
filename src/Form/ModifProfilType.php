434762d8bbaed84255ee56aa54b046780357ba49
Anto-LSR <anton1.lesueur@gmail.com>
2022-03-29 14:12:43 +0200

    update user and password

---
+++ src/Form/ModifProfilType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Participant;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ModifProfilType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom' , TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('pseudo')
+            ->add('email')
+            ->add('telephone', null, [
+                'required' => false,
+                'label' => 'Téléphone'
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe'
+            ])
+            #->add('site')
+            #->add('sorties')
+            ->add('submit', SubmitType::class, [
+                'label' => 'Modifier'
+            ])
+            ->add('Annuler', ButtonType::class)
+            ->add('ModifMDP', ButtonType::class, [
+                'label' => 'Modifier le mot de passe'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Participant::class,
+        ]);
+    }
+}
