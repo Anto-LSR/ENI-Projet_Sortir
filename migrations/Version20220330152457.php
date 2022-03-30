94da6554b25799116a043921530eedce76aff7d5
Anto-LSR <anton1.lesueur@gmail.com>
2022-03-30 17:45:13 +0200

    trouve solution js ville-lieux

---
+++ migrations/Version20220330152457.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220330152457 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE site (id INT AUTO_INCREMENT NOT NULL, nom_site VARCHAR(50) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE sortie_participant (sortie_id INT NOT NULL, participant_id INT NOT NULL, INDEX IDX_E6D4CDADCC72D953 (sortie_id), INDEX IDX_E6D4CDAD9D1C3019 (participant_id), PRIMARY KEY(sortie_id, participant_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE ville (id INT AUTO_INCREMENT NOT NULL, nom_ville VARCHAR(50) NOT NULL, code_postal INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE sortie_participant ADD CONSTRAINT FK_E6D4CDADCC72D953 FOREIGN KEY (sortie_id) REFERENCES sortie (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE sortie_participant ADD CONSTRAINT FK_E6D4CDAD9D1C3019 FOREIGN KEY (participant_id) REFERENCES participant (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE lieu ADD ville_id INT NOT NULL');
+        $this->addSql('ALTER TABLE lieu ADD CONSTRAINT FK_2F577D59A73F0036 FOREIGN KEY (ville_id) REFERENCES ville (id)');
+        $this->addSql('CREATE INDEX IDX_2F577D59A73F0036 ON lieu (ville_id)');
+        $this->addSql('ALTER TABLE participant ADD site_id INT NOT NULL, CHANGE administrateur administrateur TINYINT(1) DEFAULT false, CHANGE actif actif TINYINT(1) DEFAULT true');
+        $this->addSql('ALTER TABLE participant ADD CONSTRAINT FK_D79F6B11F6BD1646 FOREIGN KEY (site_id) REFERENCES site (id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_D79F6B1186CC499D ON participant (pseudo)');
+        $this->addSql('CREATE INDEX IDX_D79F6B11F6BD1646 ON participant (site_id)');
+        $this->addSql('ALTER TABLE sortie ADD organisateur_id INT NOT NULL, ADD site_id INT NOT NULL, ADD etat_id INT NOT NULL, ADD lieu_id INT NOT NULL, ADD motif_annulation LONGTEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE sortie ADD CONSTRAINT FK_3C3FD3F2D936B2FA FOREIGN KEY (organisateur_id) REFERENCES participant (id)');
+        $this->addSql('ALTER TABLE sortie ADD CONSTRAINT FK_3C3FD3F2F6BD1646 FOREIGN KEY (site_id) REFERENCES site (id)');
+        $this->addSql('ALTER TABLE sortie ADD CONSTRAINT FK_3C3FD3F2D5E86FF FOREIGN KEY (etat_id) REFERENCES etat (id)');
+        $this->addSql('ALTER TABLE sortie ADD CONSTRAINT FK_3C3FD3F26AB213CC FOREIGN KEY (lieu_id) REFERENCES lieu (id)');
+        $this->addSql('CREATE INDEX IDX_3C3FD3F2D936B2FA ON sortie (organisateur_id)');
+        $this->addSql('CREATE INDEX IDX_3C3FD3F2F6BD1646 ON sortie (site_id)');
+        $this->addSql('CREATE INDEX IDX_3C3FD3F2D5E86FF ON sortie (etat_id)');
+        $this->addSql('CREATE INDEX IDX_3C3FD3F26AB213CC ON sortie (lieu_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE participant DROP FOREIGN KEY FK_D79F6B11F6BD1646');
+        $this->addSql('ALTER TABLE sortie DROP FOREIGN KEY FK_3C3FD3F2F6BD1646');
+        $this->addSql('ALTER TABLE lieu DROP FOREIGN KEY FK_2F577D59A73F0036');
+        $this->addSql('DROP TABLE site');
+        $this->addSql('DROP TABLE sortie_participant');
+        $this->addSql('DROP TABLE ville');
+        $this->addSql('DROP INDEX IDX_2F577D59A73F0036 ON lieu');
+        $this->addSql('ALTER TABLE lieu DROP ville_id');
+        $this->addSql('DROP INDEX UNIQ_D79F6B1186CC499D ON participant');
+        $this->addSql('DROP INDEX IDX_D79F6B11F6BD1646 ON participant');
+        $this->addSql('ALTER TABLE participant DROP site_id, CHANGE administrateur administrateur TINYINT(1) NOT NULL, CHANGE actif actif TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE sortie DROP FOREIGN KEY FK_3C3FD3F2D936B2FA');
+        $this->addSql('ALTER TABLE sortie DROP FOREIGN KEY FK_3C3FD3F2D5E86FF');
+        $this->addSql('ALTER TABLE sortie DROP FOREIGN KEY FK_3C3FD3F26AB213CC');
+        $this->addSql('DROP INDEX IDX_3C3FD3F2D936B2FA ON sortie');
+        $this->addSql('DROP INDEX IDX_3C3FD3F2F6BD1646 ON sortie');
+        $this->addSql('DROP INDEX IDX_3C3FD3F2D5E86FF ON sortie');
+        $this->addSql('DROP INDEX IDX_3C3FD3F26AB213CC ON sortie');
+        $this->addSql('ALTER TABLE sortie DROP organisateur_id, DROP site_id, DROP etat_id, DROP lieu_id, DROP motif_annulation');
+    }
+}
